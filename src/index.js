b5f6811265bd06f8674f95dcc3ad8bde82b1f8ef
Rew <irinavakulenko2000@gmail.com>
2023-09-18 17:07:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='car-rent'>
+    <BrowserRouter basename='car-rent-project'>
         <App classname='App' />
     </BrowserRouter>
   </React.StrictMode>
